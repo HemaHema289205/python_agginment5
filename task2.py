e0199d91c4087dca82927514e1c641442420c76d
Hema kota nagu <gudimetlahemakotanagu@gmail.com>
2025-06-08 21:12:39 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print(f"original list: {list1}")
+extract_list=list1[:5]
+print(f"Extracted first five elements: {extract_list}")
+reversed_list=extract_list[::-1]
+print(f"Reversed extracted elements: {reversed_list}")
